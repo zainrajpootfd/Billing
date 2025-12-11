0c9b27cacae2d3c54d0b267ad6b0a24990b0bfac
rana zain ali <zainrajpootfd@gmail.com>
2025-12-11 17:36:33 +0500

    refactor: remove MaintenanceWrapper component from Home page for streamlined layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,12 +18,10 @@ import KeyDifferentiators from "@/components/landing/KeyDifferentiators";
 import IntegrationPartners from "@/components/landing/IntegrationPartners";
 import CaseStudies from "@/components/landing/CaseStudies";
 import HowToGetStarted from "@/components/landing/HowToGetStarted";
-import MaintenanceWrapper from "@/components/landing/MaintenanceWrapper";
 
 export default function Home() {
   return (
     <div className="min-h-screen mx-auto">
-      <MaintenanceWrapper>
         {/* <TopBar />
       <Header /> */}
         <Hero />
@@ -44,7 +42,6 @@ export default function Home() {
         <FAQ />
         <Contact />
         {/* <Footer /> */}
-      </MaintenanceWrapper>
     </div>
   );
 }
